7c953ebddab61821ea6e20ac41622eceab4732b6
Muhammad Owais <awaisdev2@gmail.com>
2025-09-02 17:34:45 +0500

    Revert "format layout"
    This reverts commit 2a149fe1215e6794fbceb306bf870645f942c75b.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,11 @@
 import { Stack } from "expo-router";
-import "react-native-reanimated";
+import 'react-native-reanimated';
 import "./global.css";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
   );
 }
